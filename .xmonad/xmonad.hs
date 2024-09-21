0a6945cf653918a9376fa2b3afd0bfde9a50b9d2
Ilona <ixahedron@users.noreply.github.com>
2024-09-21 13:42:29 +0200

    bump

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
@@ -16,14 +17,13 @@ bordWidth = 1
 normColour = "#cccccc"
 bordColour = "#cd8b00"
 
-addKeyBindings = [ ((mod4Mask .|. shiftMask, xK_z), spawn "slimlock")
-                 , ((controlMask, xK_Print), spawn "scrot")
-                 , ((0, xK_Print), spawn "sleep 0.2; scrot -s")
+addKeyBindings = [ ((controlMask, xK_Print), spawn "escrotum")
+                 , ((0, xK_Print), spawn "sleep 0.2; escrotum -s")
 --                 , ((0, 0x1008ff03), spawn "light -U 10")
 --                 , ((0, 0x1008ff02), spawn "light -A 10")
-                 , ((0, 0x1008ff11), spawn "pactl set-sink-volume 0 -5%")
-                 , ((0, 0x1008ff12), spawn "pactl list sinks | grep -q Mute:.no && pactl set-sink-mute 0 1 || pactl set-sink-mute 0 0")
-                 , ((0, 0x1008ff13), spawn "pactl set-sink-mute 0 0 | pactl set-sink-volume 0 +5%")
+                 , ((0, 0x1008ff11), spawn "for i in {0..10}; do pactl set-sink-volume $i -5%; done")
+                 , ((0, 0x1008ff12), spawn "pactl list sinks | grep -q Mute:.no && pactl set-sink-mute 1 1 || pactl set-sink-mute 1 0")
+                 , ((0, 0x1008ff13), spawn "for i in {0..10}; do pactl set-sink-mute $i 0 | pactl set-sink-volume $i +5%; done")
 --                 , ((mod4Mask .|. controlMask              , xK_plus ), sendMessage MagnifyMore)
 --                 , ((mod4Mask .|. controlMask              , xK_minus), sendMessage MagnifyLess)
 --                 , ((mod4Mask .|. controlMask              , xK_o    ), sendMessage ToggleOff  )
@@ -37,7 +37,7 @@ magnifyLayout = magnifier (Tall 1 (3/100) (1/2))
 
 main = do
   xmproc <- spawnPipe xmobar
-  xmonad $ docks def
+  xmonad $ ewmhFullscreen $ ewmh $ docks def
       { manageHook         = manageDocks <+> manageHook def
       , layoutHook         = avoidStruts $ layoutHook def --{layoutHook = magnifyLayout}
       , terminal           = myTerminal
